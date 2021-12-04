53be2832b2735f0853709fb46a9fb0aaf0a8c36d
ashkan 1377 <ashkanmogharab@gmail.com>
2021-12-05 02:05:02 +0330

    an instance of User created and printed its details

--- src/ServerSide.java
+++ src/ServerSide.java
@@ -1,9 +1,11 @@
+import java.time.*;
 /**
  * this class holds details of a ServerSide
  * @author ashkan_mogharab
  */
 public class ServerSide{
     public static void main(String[]args){
-
+        User user1 = new User("ashkan","mogharab","ashkan1377","ashkan13771998", LocalDate.of(1377,10,7),LocalDate.of(1400,9,14),"deymahi");
+        System.out.println(user1.toString());
     }
 }
